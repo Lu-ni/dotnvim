2ac249729ecc682807fb75f0d4348105794bfd08
Lucas Nicollier <lucas.nicollier@gmail.com>
2024-07-29 14:09:45 +0200

    added harpoon and undotree

--- lua/myconfig/lazy.lua
+++ lua/myconfig/lazy.lua
@@ -12,15 +12,33 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{'nvim-telescope/telescope.nvim', tag = '0.1.8'},
-	{"rose-pine/neovim", name = "rose-pine"},
-	{ "EdenEast/nightfox.nvim", name= "nightfox" },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    {'nvim-telescope/telescope.nvim', tag = '0.1.8'},
+    {
+        "rose-pine/neovim",
+        name = "rose-pine"
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        name= "nightfox"
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x'
+    },
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {"mbbill/undotree"},
 })
