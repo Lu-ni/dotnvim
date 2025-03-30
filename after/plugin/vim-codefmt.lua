ee561ad18f42983a99a09ff0a50ccce05e193bed
Lucas Nicollier <lucas.nicollier@syncai.ch>
2025-03-30 16:34:38 +0200

    remove python autoformat

--- after/plugin/vim-codefmt.lua
+++ after/plugin/vim-codefmt.lua
@@ -9,12 +9,6 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  command = "AutoFormatBuffer black",
-  group = autoformat_settings,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
   pattern = "sql",
   command = "AutoFormatBuffer pg_format",
   group = autoformat_settings,
